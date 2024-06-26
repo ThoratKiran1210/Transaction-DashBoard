4804958c95fa60b802c3a152cdaabf2c8620662f
Kiran Thorat <127298235+ThoratKiran1210@users.noreply.github.com>
2024-06-26 18:31:50 +0530

    Update SearchBox.js

--- SearchBox.js
+++ SearchBox.js
@@ -1,20 +1,3 @@
-// import React from "react";
-
-// const SearchBox = ({ searchQuery, setSearchQuery }) => {
-//   return (
-//     <input
-//       type="text"
-//       value={searchQuery}
-//       onChange={(e) => setSearchQuery(e.target.value)}
-//       placeholder="Search transactions..."
-//     />
-//   );
-// };
-
-// export default SearchBox;
-
-
-// src/components/SearchBox.js
 import React from 'react';
 import './SearchBox.css';
 
