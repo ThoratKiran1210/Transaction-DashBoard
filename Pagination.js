2bb76612bb2bb409ca9390fec3b4573ee292d65e
Kiran Thorat <127298235+ThoratKiran1210@users.noreply.github.com>
2024-06-26 18:32:08 +0530

    Update Pagination.js

--- Pagination.js
+++ Pagination.js
@@ -1,32 +1,3 @@
-// import React from "react";
-
-// const Pagination = ({ currentPage, setCurrentPage, totalPages }) => {
-//   return (
-//     <div>
-//       <button
-//         onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-//         disabled={currentPage === 1}
-//       >
-//         Previous
-//       </button>
-//       <span>
-//         Page {currentPage} of {totalPages}
-//       </span>
-//       <button
-//         onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
-//         disabled={currentPage === totalPages}
-//       >
-//         Next
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default Pagination;
-
-
-
-// src/components/Pagination.js
 import React from 'react';
 import './Pagination.css';
 
